4f60f245707b2a7c978bb99f9d86d293103799a0
wayTo <yurigarciadev@gmail.com>
2024-09-01 16:38:30 -0300

    setup https

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
 import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  let httpsOptions: { key: Buffer; cert: Buffer } | undefined;
+  if (
+    fs.existsSync('etc/letsencrypt/live/alabarda.link/privkey.pem') &&
+    fs.existsSync('etc/letsencrypt/live/alabarda.link/fullchain.pem')
+  ) {
+    httpsOptions = {
+      key: fs.readFileSync('etc/letsencrypt/live/alabarda.link/privkey.pem'),
+      cert: fs.readFileSync('etc/letsencrypt/live/alabarda.link/fullchain.pem'),
+    };
+  }
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
