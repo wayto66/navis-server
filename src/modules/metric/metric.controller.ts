922688ff0dcd31be82d15c7d69503f22424f51d3
wayTo <yurigarciadev@gmail.com>
2024-09-28 14:59:07 -0300

    prom-client

--- src/modules/metric/metric.controller.ts
+++ src/modules/metric/metric.controller.ts
@@ -10,8 +10,10 @@ export class MetricController {
   constructor(private readonly metricService: MetricService) {}
 
   @Get()
-  isOnline(@Res() res: Response) {
-    res.set('Content-Type', register.contentType);
-    return this.metricService.isOnline();
+  async isOnline(@Res() res: Response) {
+    // Call the service to get the metrics
+    const metrics = await this.metricService.isOnline();
+    res.set('Content-Type', register.contentType); // Set the content type here
+    return res.send(metrics); // Send the metrics as the response
   }
 }
