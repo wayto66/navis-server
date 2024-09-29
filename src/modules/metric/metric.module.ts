159263a91f9931c910ea63e4274f13049074f369
wayTo <yurigarciadev@gmail.com>
2024-09-29 18:46:13 -0300

    prometheus changes

--- src/modules/metric/metric.module.ts
+++ src/modules/metric/metric.module.ts
@@ -1,9 +1,11 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 
 import { MetricMiddleware } from '../../infra/middlewares/metric.middleware';
+import { MetricController } from './metric.controller';
 import { MetricService } from './metric.service';
 
 @Module({
+  controllers: [MetricController],
   providers: [MetricService],
   exports: [MetricService],
 })
